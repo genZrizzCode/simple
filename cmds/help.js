9d994cef44d10447dd0dc5c80f607574c039a4e2
Maxim Varshavsky <code4rizz@gmail.com>
2025-07-20 14:32:47 -0700

    First Major Update Ever

---
+++ cmds/help.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log("Main Commands:\n\n")
+console.log("Hello: The default command to see if this CLI works or not. It is also the only command that starts with an uppercase letter. Confusing, right?\n")
+console.log("help: Shows the important commands you need to know.\n")
--- index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-console.log("Hello from the simple CLI!\nIf you need help, type help then press enter.");
