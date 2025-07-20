a796adca10e2acc4ef48930ebfbad5be6a1f1a7b
Maxim Varshavsky <code4rizz@gmail.com>
2025-07-20 15:14:07 -0700

    AHHHH hate bug fixes

--- cmds/ping.js
+++ cmds/ping.js
@@ -3,22 +3,22 @@
 import chalk from 'chalk';
 import { exec } from 'child_process';
 
-const target = process.argv[2] || 'example.org';
+export default function ([target = 'example.org']) {
+  // Run the ping command with 1 packet
+  exec(`ping -c 1 ${target}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(chalk.red('ERROR: Ping failed:'), stderr.trim());
+      process.exit(1);
+    }
 
-// Run the ping command with 1 packet
-exec(`ping -c 1 ${target}`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(chalk.red('ERROR: Ping failed:'), stderr.trim());
-    process.exit(1);
-  }
-
-  // Look for the time=123.456 ms part
-  const match = stdout.match(/time: ([\d.]+)ms/);
-  if (match) {
-    const pingTime = match[1];
-    console.log(chalk.bold.green(`PONG 🏓`), chalk.white(` — ${pingTime}ms to ${target}`));
-  } else {
-    console.log(chalk.bold.yellow('Ping succeeded, but couldn’t parse the time.'));
-    console.log(stdout);
-  }
-});
+    // Look for the time=123.456 ms part
+    const match = stdout.match(/time=([\d.]+) ms/);
+    if (match) {
+      const pingTime = match[1];
+      console.log(chalk.bold.green(`PONG 🏓`), chalk.white(` — ${pingTime}ms to ${target}`));
+    } else {
+      console.log(chalk.bold.yellow('Ping succeeded, but couldn’t parse the time.'));
+      console.log(stdout);
+    }
+  });
+}
