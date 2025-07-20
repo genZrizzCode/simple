dc524c7a83a0e45c5f2a462584de1cb5ecac2cbb
Maxim Varshavsky <code4rizz@gmail.com>
2025-07-20 14:51:41 -0700

    fix stuff

--- cmds/hello.js
+++ cmds/hello.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-console.log("Hello from the simple CLI!\nIf you need help, type help then press enter.\n");
+console.log("Hello from the simple CLI!\nIf you need help, type 'simple help' then press enter.\n");
--- cmds/help.js
+++ cmds/help.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-console.log("Main Commands:\n\n")
+import chalk from "chalk"
+console.log(chalk.bold("Main Commands:\n\n"))
 console.log("Hello: The default command to see if this CLI works or not. It is also the only command that starts with an uppercase letter. Confusing, right?\n")
 console.log("help: Shows the important commands you need to know.\n")
