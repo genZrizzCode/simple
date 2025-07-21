9aa976bc8c6a360e2faca03e1f984ce6df2782ff
Maxim Varshavsky <code4rizz@gmail.com>
2025-07-20 22:04:18 -0700

    Very minor bug fix (see how it's not plural)

--- cmds/random-int.js
+++ cmds/random-int.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 export default function(args) {
-  if (!args || args.length === 0) {
-    console.log('Usage: echo <string>');
+  if (!args || args.length < 2) {
+    console.log('Usage: random-int <min> <max>');
     process.exit(1);
   }
-  const string = args.join(' ');
-  console.log(string);
+  const min = parseInt(args[0]);
+  const max = parseInt(args[1]);
+  if (isNaN(min) || isNaN(max)) {
+    console.log('Both min and max must be numbers.');
+    process.exit(1);
+  }
+  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 }
