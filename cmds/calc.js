509c8c26a35e05c3046cfdba46b1c6ebb26369ca
Maxim Varshavsky <code4rizz@gmail.com>
2025-07-20 21:21:51 -0700

    Minor bug fixes

--- cmds/calc.js
+++ cmds/calc.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log('Usage: calc <expression>');
-  process.exit(1);
+export default function(args) {
+  if (!args || args.length === 0) {
+    console.log('Usage: calc <expression>');
+    process.exit(1);
+  }
+  const expression = args.join(' ');
+  const result = eval(expression);
+  console.log(result);
 }
-
-const expression = args.join(' ');
-const result = eval(expression);
-console.log(result);
